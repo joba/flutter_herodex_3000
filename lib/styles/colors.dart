e47204fe60a65b29556cd52fa3845cb45fabdc20
Jon Bågling <bagling@gmail.com>
2026-02-03 09:15:42 +0100

    update error color

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color onPrimary = Color(0xFF0A0C0A);
   static const Color secondary = Color(0xCC4ADE80);
   static const Color onSecondary = Color(0xFF0A0C0A);
-  static const Color error = Color(0xFFB00020);
+  static const Color error = Color(0xFFDC2626);
   static const Color onError = Color(0xFFFFFFFF);
   static const Color surface = Color(0xFF121212);
   static const Color onSurface = Color(0xFFFFFFFF);
